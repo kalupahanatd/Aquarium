67f0d18c644913c4851a907651a98c2ad8df1263
kalupahanatd <67464246+kalupahanatd@users.noreply.github.com>
2020-08-30 09:46:56 +0530

    added user model

---
+++ Aquarium/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aquarium.Models
+{
+    
+        public interface IUserService
+        {
+            List<Fish> GetAll();
+            Fish Get(int id);
+
+            void Add(Fish newfish);
+            void Update(int id, Fish book);
+            void Delete(int id);
+        }
+    
+}
